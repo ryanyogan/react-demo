c09c162030a1305563522fd71fafcfe25dcce9d2
Ryan Yogan <ryanyogan@gmail.com>
2014-10-02 17:57:52 -0500

    Added a comment list with fake data

--- app/assets/javascripts/comment.js.jsx
+++ app/assets/javascripts/comment.js.jsx
@@ -13,9 +13,30 @@ var Comment = React.createClass({
   }
 });
 
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.comments.map(function(comment, index) {
+      return (
+        <Comment author={comment.author} comment={comment.comment} key={index} />
+      );
+    });
+
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
 var renderReact = function() {
+  var fakeComments = [
+    { author: "Ryan", comment: "This is a comment." },
+    { author: "Moose", comment: "This is *another* comment" }
+  ];
+
   React.renderComponent(
-    <Comment author="Richard" comment="This is a comment"/>,
+    <CommentList comments={fakeComments} />,
     document.getElementById('comments')
   );
 };
