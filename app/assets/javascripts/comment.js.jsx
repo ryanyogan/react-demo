3de61f562040edeacfc0f1d8a533f23dd320283b
Ryan Yogan <ryanyogan@gmail.com>
2014-10-02 17:53:53 -0500

    Added a comment react component

--- app/assets/javascripts/comment.js.jsx
+++ app/assets/javascripts/comment.js.jsx
@@ -1,18 +1,21 @@
 /** @jsx React.DOM */
 
-var HelloWorld = React.createClass({
+var Comment = React.createClass({
   render: function() {
     return (
-      <div className='HelloWorld'>
-        Hello, World!
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+          {this.props.comment}
       </div>
     );
   }
 });
 
-var renderReact = function () {
+var renderReact = function() {
   React.renderComponent(
-    <HelloWorld />,
+    <Comment author="Richard" comment="This is a comment"/>,
     document.getElementById('comments')
   );
 };
